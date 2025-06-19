480ae4332b01b39bd3805baa3d5f17180a5c94b0
Rao Zohaib <zohaibrao630@gmail.com>
2025-06-19 10:50:12 -0700

    sitemap-commit

--- PortfolioApp/urls.py
+++ PortfolioApp/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import HomeView, ThankView
+from django.views.generic import TemplateView
 app_name = 'PortfolioApp'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('thanks/', ThankView.as_view(), name='thanks'),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path("sitemap.xml", TemplateView.as_view(
+        template_name="sitemap.xml",
+        content_type="application/xml"
+    )),
 ]
